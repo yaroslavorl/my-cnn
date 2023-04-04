3910452b77e33e7e29e7a2b389b54d9b8f769dde
yaroslavorl <jaroslavorlenko@yandex.ru>
2023-04-04 16:20:09 +0300

    new class polling

--- model_class.py
+++ model_class.py
@@ -1,22 +1,26 @@
-from class_cnn.conv_class import Conv2D
+from class_cnn.nn_class import Conv2D, Poling
 import numpy as np
 
 
 class ModelCnn(object):
 
     def __init__(self):
-        self.layers_1 = Conv2D(1, kernel_size=2, input_size=5)
+        self.layers_1 = Conv2D(2, kernel_size=3, input_size=32)
+        self.pool = Poling(30, 2, pool_size=2, method='min')
 
     def predict(self, x):
-        return self._relu(self.layers_1(x))
+        x = self.layers_1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        return x
 
     @staticmethod
-    def _relu(x):
+    def relu(x):
         return np.maximum(0, x)
 
 
 if __name__ == "__main__":
-    img = np.random.randint(0, 256, (1, 5, 5))
+    img = np.random.randint(0, 255, (1, 32, 32))
     model = ModelCnn()
     print(model.predict(img).shape)
 
---
+++ nn_class.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+class Conv2D(object):
+
+    def __init__(self, filters, kernel_size, input_size):
+        self.weights = np.random.uniform(0, 1, (filters, kernel_size, kernel_size))
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.input_size = input_size
+
+    def __call__(self, x, *args, **kwargs):
+        return self._conv(x, self.input_size, self.kernel_size)
+
+    def _conv(self, x, input_size, kernel_size):
+        output = np.zeros((self.filters, input_size - kernel_size + 1, input_size - kernel_size + 1))
+
+        for k in range(input_size - kernel_size + 1):
+            for m in range(input_size - kernel_size + 1):
+                output[:, k, m] = np.sum(x[0, k:kernel_size + k, m:kernel_size + m] * self.weights, axis=(1, 2))
+
+        return output
+
+
+class Poling(object):
+    METHOD = {'max': np.max, 'min': np.min, 'avg': np.average}
+
+    def __init__(self, input_size, input_channels, pool_size, method='max'):
+        self.pool_size = pool_size
+        self.input_size = input_size
+        self.input_channels = input_channels
+        self.method = self.METHOD[method]
+
+    def __call__(self, x, *args, **kwargs):
+        return self.poling(x)
+
+    def poling(self, x):
+        output_size = self.input_size // self.pool_size
+        output = np.zeros((self.input_channels, output_size, output_size))
+
+        for channel in range(self.input_channels):
+            for i, k in zip(range(0, self.input_size, self.pool_size), range(output_size)):
+                for j, m in zip(range(0, self.input_size, self.pool_size), range(output_size)):
+                    output[channel, k, m] = self.method(x[channel, i:self.pool_size + i, j:self.pool_size + j])
+        return output
